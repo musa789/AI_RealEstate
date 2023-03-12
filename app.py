7a4c6b57e3023cb01fc3dda9dd9f9a7e66fdb09e
musa789 <musamazhar@gmail.com>
2023-03-12 16:21:54 +0500

    Add AI algorithm for predicting real estate prices

--- app.py
+++ app.py
@@ -6,29 +6,58 @@ def predict_location(data, budget, city, property_type, period, access_token):
     filtered_data = filter_data(data, budget, city, property_type)
 
     predicted_price_period = predict_price(filtered_data, period)
+
     # Create map centered on the first property in filtered_data
     center_lat = filtered_data.iloc[0]['latitude']
     center_lon = filtered_data.iloc[0]['longitude']
     map_location = [center_lat, center_lon]
-    m = folium.Map(location=map_location, zoom_start=15, tiles='https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', attr='Mapbox', id='mapbox/streets-v11')
-    # Add markers to the map for each property in filtered_data
-    for index, row in filtered_data.iterrows():
-        price = row['price']
-        lat = row['latitude']
-        lon = row['longitude']
-        tooltip = f"Price: {price} PKR"
-        folium.Marker([lat, lon], tooltip=tooltip).add_to(m)
-    # Add marker for predicted location
+
+    # Generate Mapbox access token
+    px.set_mapbox_access_token(access_token)
+
+    # Create a scatter plot of properties in the filtered data
+    fig = px.scatter_mapbox(filtered_data,
+                            lat='latitude',
+                            lon='longitude',
+                            size='price',
+                            color='price',
+                            hover_name='title',
+                            hover_data=['price', 'beds', 'baths'],
+                            zoom=12,
+                            center=dict(lat=center_lat, lon=center_lon))
+
+    # Add a marker for the predicted location
     predicted_lat = filtered_data.iloc[0]['latitude'] + 0.001
     predicted_lon = filtered_data.iloc[0]['longitude'] + 0.001
-    predicted_tooltip = f"Predicted Price: {predicted_price_period:.2f} PKR"
-    folium.Marker([predicted_lat, predicted_lon], tooltip=predicted_tooltip, icon=folium.Icon(color='green')).add_to(m)
-    # Save map to temporary HTML file
+    predicted_price_str = f"Predicted Price: {predicted_price_period:.2f} PKR"
+    fig.add_trace(
+        go.Scattermapbox(
+            lat=[predicted_lat],
+            lon=[predicted_lon],
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                size=10,
+                color='green'
+            ),
+            text=predicted_price_str,
+            hoverinfo='text'
+        )
+    )
+
+    # Set the layout of the figure
+    fig.update_layout(
+        mapbox=dict(
+            style='streets',
+            center=dict(lat=center_lat, lon=center_lon),
+            zoom=12,
+        ),
+        height=600,
+    )
+
+    # Save the map to a temporary HTML file
     temp_file = tempfile.NamedTemporaryFile(suffix='.html', delete=False)
     temp_file.close()
-    m.save(temp_file.name)
+    fig.write_html(temp_file.name)
 
-    # Open map in browser and display predicted price
-    mapbox_url = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/{lon},{lat},{zoom}/{width}x{height}?access_token={access_token}'
-    map_url = mapbox_url.format(lon=center_lon, lat=center_lat, zoom=15, width=600, height=400, access_token=access_token)
-    return render_template('predict.html', result=f"The predicted price for a {property_type.lower()} in {city} with a period of {period} months is approximately {predicted_price_period:.2f} PKR.", map_url=map_url)
+    # Open the map in a new tab and display the predicted price
+    return f"The predicted price for a {property_type.lower()} in {city} with a period of {period} months is approximately {predicted_price_period:.2f} PKR. <br> <a href='{temp_file.name}' target='_blank'>View Map</a>"
